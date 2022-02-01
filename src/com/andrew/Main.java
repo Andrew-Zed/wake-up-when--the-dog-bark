b15c3d355ca08358285a037406018985b344f8ce
Andrew-Zed <andrew.awadyke@gmail.com>
2022-02-01 21:23:13 +0100

    LeapYear Challenge added

--- src/com/andrew/Main.java
+++ src/com/andrew/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         BarkingDog bk = new BarkingDog();
         bk.shouldWakeUp(false,-1);
+
+        LeapYear lp = new LeapYear();
+        lp.isLeapYear(-1600);
     }
 }
